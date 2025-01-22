6aa7e58bf061a3a88e9a78e9057b8e906ee3cd79
Fadhil Yori <me@fadhilyori.my.id>
2025-01-22 15:52:17 +0700

    Add initial implementation of report command service with configuration and logging

---
+++ cmd/report/root.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mata-elang-stable/report-command-service/internal/config"
+	"github.com/mata-elang-stable/report-command-service/internal/logger"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	appVersion = "dev"
+	appCommit  = "none"
+	appLicense = "MIT"
+)
+
+var log = logger.GetLogger()
+
+var rootCmd = &cobra.Command{
+	Use:   "mea-report",
+	Short: "Mata Elang Addon Report Command Service",
+	Long: `Mata Elang Addon Report Command Service is a service that listens to incoming messages from Kafka,
+processes them, and sends the result to another Kafka topic.`,
+	Run:  runApp,
+	Args: cobra.NoArgs,
+}
+
+func init() {
+	// Read configuration from .env file in the current directory
+	// viper.SetConfigFile("./.env")
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.WithField("error", err).Warnln("Failed to read configuration file")
+	}
+
+	viper.SetEnvPrefix("app")
+	viper.AutomaticEnv()
+
+	conf := config.GetConfig()
+
+	viper.SetDefault("kafka_brokers", "localhost:9092")
+	viper.SetDefault("kafka_topic_input", "sensor_events")
+	viper.SetDefault("kafka_topic_output", "me_report_aggregated")
+	viper.SetDefault("schema_registry_url", "http://localhost:8081")
+
+	if err := viper.Unmarshal(&conf); err != nil {
+		log.Fatalf("Failed to unmarshal configuration: %v", err)
+	}
+
+	flags := rootCmd.PersistentFlags()
+
+	flags.StringVar(&conf.KafkaBrokers, "kafka-brokers", conf.KafkaBrokers, "")
+	flags.StringVar(&conf.InputKafkaTopic, "input-topic", conf.InputKafkaTopic, "")
+	flags.StringVar(&conf.SchemaRegistryUrl, "schema-registry-url", conf.SchemaRegistryUrl, "")
+	flags.StringVar(&conf.OutputKafkaTopic, "output-topic", conf.OutputKafkaTopic, "")
+	flags.CountVarP(&conf.VerboseCount, "verbose", "v", "Increase verbosity of the output.")
+
+	if err := viper.BindPFlags(flags); err != nil {
+		log.WithField("error", err).Fatalln("Failed to bind flags.")
+	}
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ internal/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"sync"
+
+	"github.com/mata-elang-stable/report-command-service/internal/logger"
+)
+
+type Config struct {
+	// SchemaRegistryUrl is the schema registry URL.
+	SchemaRegistryUrl string `mapstructure:"schema_registry_url"`
+
+	// KafkaBrokers is the Kafka broker to connect to.
+	KafkaBrokers string `mapstructure:"kafka_brokers"`
+
+	// InputKafkaTopic is the Kafka topic.
+	InputKafkaTopic string `mapstructure:"kafka_topic_input"`
+
+	// OutputKafkaTopic is the Kafka topic.
+	OutputKafkaTopic string `mapstructure:"kafka_topic_output"`
+
+	// VerboseCount is the verbose level.
+	VerboseCount int `mapstructure:"verbose"`
+}
+
+var log = logger.GetLogger()
+
+var instance *Config
+var once sync.Once
+
+func GetConfig() *Config {
+	once.Do(func() {
+		instance = &Config{}
+	})
+
+	return instance
+}
+
+func (c *Config) SetupLogging() {
+	switch instance.VerboseCount {
+	case 0:
+		log.SetLevel(logger.InfoLevel)
+	case 1:
+		log.SetLevel(logger.DebugLevel)
+	default:
+		log.SetLevel(logger.TraceLevel)
+	}
+	log.WithFields(logger.Fields{
+		"LOG_LEVEL": log.GetLevel().String(),
+	}).Infoln("Logging level set.")
+}
