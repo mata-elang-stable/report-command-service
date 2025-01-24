88d3819e77cd2505c749b1bef0a4fb592b02cf06
Fadhil Yori <me@fadhilyori.my.id>
2025-01-24 17:57:08 +0700

    Refactor Metric and Event types for improved structure and JSON serialization

--- internal/types/types.go
+++ internal/types/types.go
@@ -2,38 +2,53 @@ package types
 
 import "sync"
 
-type Metric struct {
+type MetricDraft struct {
 	Count           uint32
 	SnortDstAddress *string
 	SnortSrcAddress *string
-	SnortDstSrcPort map[string]uint32
-	mu              sync.RWMutex
+	SnortDstSrcPort sync.Map
 }
 
-func (m *Metric) StoreOrIncrementDstSrcPort(key string) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+type Metric struct {
+	Count           uint32 `json:"count"`
+	SnortDstAddress *string `json:"snort_dst_address"`
+	SnortSrcAddress *string `json:"snort_src_address"`
+	SnortDstSrcPort map[string]uint32 `json:"snort_dst_src_port"`
+}
 
-	if m.SnortDstSrcPort == nil {
-		m.SnortDstSrcPort = make(map[string]uint32)
-	}
-
-	if _, ok := m.SnortDstSrcPort[key]; ok {
-		m.SnortDstSrcPort[key]++
-	} else {
-		m.SnortDstSrcPort[key] = 1
+func (m *MetricDraft) StoreOrIncrementDstSrcPort(key string) {
+	d, loaded := m.SnortDstSrcPort.LoadOrStore(key, 1)
+	if loaded {
+		m.SnortDstSrcPort.Store(key, d.(int)+1)
 	}
 
 	m.Count++
 }
 
+func (m *MetricDraft) ToMetric() Metric {
+	metric := Metric{
+		Count:           m.Count,
+		SnortDstAddress: m.SnortDstAddress,
+		SnortSrcAddress: m.SnortSrcAddress,
+		SnortDstSrcPort: make(map[string]uint32),
+	}
+
+	m.SnortDstSrcPort.Range(func(key, value interface{}) bool {
+		metric.SnortDstSrcPort[key.(string)] = uint32(value.(int))
+		return true
+	})
+
+	return metric
+}
+
 type Event struct {
-	EventMetricsCount   uint32
-	SensorID            string
-	SnortPriority       string
-	SnortClassification string
-	SnortMessage        string
-	SnortProtocol       string
-	SnortSeconds        int64
-	Metrics             []*Metric
+	KeyHash             string `json:"key_hash"`
+	EventMetricsCount   uint32 `json:"event_metrics_count"`
+	SensorID            string `json:"sensor_id"`
+	SnortPriority       string `json:"snort_priority"`
+	SnortClassification string `json:"snort_classification"`
+	SnortMessage        string `json:"snort_message"`
+	SnortProtocol       string `json:"snort_protocol"`
+	SnortSeconds        int64 `json:"snort_seconds"`
+	Metrics             []Metric `json:"metrics"`
 }
